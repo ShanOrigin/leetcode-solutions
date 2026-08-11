aa960610fa372053a94403da54b189bc5f87546d
Suryawanshi Shantanu <shantanusuryawanshi3.14@gmail.com>
2026-08-11 22:51:48 +0530

    Add solution: Reverse String

---
+++ solutions/Easy/reverse-string/reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int size = s.size( ) ;
+        if( size == 1 ) return ;
+        int left = 0 , right = size - 1  ; 
+        while( left < right ){
+            int temp = s[left];
+            s[left]  = s[right];
+            s[right] = temp ;
+            ++left;
+            --right;
+        }
+    }
+};
